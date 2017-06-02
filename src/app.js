e517c2e3dda89a6dcf5b669579b726ebb96913c7
Christophe Geers <christophe.geers@kbc.be>
2017-06-02 14:55:03 +0200

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const Alexa = require('alexa-sdk');
 const TOO_LOW = '{number} is too low';
 const TOO_HIGH = 'Just like you in Amsterdam last week, this {number} is too high';
 const CORRECT = '{number} is correct! Would you like to play a new game?';
-const START = 'Great! Try saying a number to start the game.';
 const THANK_YOU_COME_AGAIN = 'Thank you! Come again.';``
 
 var states = {
@@ -30,14 +29,14 @@ module.exports.handlers = {
 module.exports.startModeHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
     Start() {
         this.emit(
-          ':tell',
+          ':ask',
           `Welcome to High Lowe guessing game. You have played ${this.attributes.gamesPlayed.toString()}  times. Would  you like to play?`
         );
     },
     'AMAZON.YesIntent': function () {
         this.attributes.guessNumber = Math.floor(Math.random() * 100);
         this.handler.state = states.GUESSMODE;
-        this.emit(':ask', START);
+        this.emit(':ask', 'Great! Try saying a number to start the game.');
     },
     'AMAZON.NoIntent': function () {
         this.emit(':tell', THANK_YOU_COME_AGAIN);
